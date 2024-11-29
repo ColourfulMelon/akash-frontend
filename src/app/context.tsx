1add0129ffd8ea1fcffb0e5fc0f0619e18de086d
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 05:31:47 +0200

    feat: Setup tanstack query

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { AppSidebar } from '@/components/app-sidebar';
 import React from 'react';
 import { ThemeProvider } from '@/components/theme-provider';
 import { SidebarProvider } from '@/components/ui/sidebar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export function ContextProvider({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
@@ -11,10 +16,12 @@ export function ContextProvider({ children }: Readonly<{ children: React.ReactNo
             enableSystem
             disableTransitionOnChange
         >
-            <SidebarProvider defaultOpen={false}>
-                <AppSidebar/>
-                {children}
-            </SidebarProvider>
+            <QueryClientProvider client={queryClient}>
+                <SidebarProvider defaultOpen={false}>
+                    <AppSidebar/>
+                    {children}
+                </SidebarProvider>
+            </QueryClientProvider>
         </ThemeProvider>
     );
 }
