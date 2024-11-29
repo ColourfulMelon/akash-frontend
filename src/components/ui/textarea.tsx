3d0da189682ddac7f34c060951f70745f5024e82
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 17:17:32 +0200

    chore: remove unused import

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-import {render} from "react-dom";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
