6764764e8e8140a47acef9413304a862f664e528
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 02:15:51 +0200

    feat: Add gradient config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,129 +1,148 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
-  theme: {
-  	container: {
-  		center: true,
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	fontFamily: {
-  		poppins: ['Poppins', 'sans-serif'],
-  		SFPro: ['SF Pro Display', 'sans-serif']
-  	},
-  	extend: {
-  		boxShadow: {
-  			'red-glow': '0 4px 100px rgba(255, 0, 0, 0.6)'
-  		},
-  		colors: {
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-		    fadeOut: {
-			    '0%': { opacity: '1' },
-			    '100%': { opacity: '0' },
-		    },
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			glow: {
-  				'0%, 100%': {
-  					boxShadow: '0 4px 15px rgba(255, 0, 0, 0)'
-  				},
-  				'50%': {
-  					boxShadow: '0 4px 15px rgba(255, 0, 0, 0.6)'
-  				}
-  			},
-		    moveToBottom: {
-			    '0%': { transform: 'translateY(0)' },
-			    '100%': { transform: 'translateY(22vh)' },
-		    },
-		    fadeIn: {
-			    '0%': { opacity: '0', transform: 'scale(0.95)' },
-			    '100%': { opacity: '1', transform: 'scale(1)' },
-		    },
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			glow: 'glow 10s infinite',
-		    fadeOut: 'fadeOut 3s 2s forwards',
-		    moveToBottom: 'moveToBottom 1s forwards',
-		    fadeIn: 'fadeIn 1s ease-out forwards',
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+    darkMode: ['class'],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    prefix: '',
+    theme: {
+    	container: {
+    		center: true,
+    		padding: '2rem',
+    		screens: {
+    			'2xl': '1400px'
+    		}
+    	},
+    	fontFamily: {
+    		poppins: ['Poppins', 'sans-serif'],
+    		SFPro: ['SF Pro Display', 'sans-serif']
+    	},
+    	extend: {
+    		backgroundImage: {
+    			'radial-gradient': 'radial-gradient(circle at bottom right, var(--tw-gradient-stops))'
+    		},
+    		boxShadow: {
+    			'red-glow': '0 4px 100px rgba(255, 0, 0, 0.6)'
+    		},
+    		colors: {
+    			'bg-gradient-start': 'hsl(var(--bg-gradient-start))',
+    			'bg-gradient-end': 'hsl(var(--bg-gradient-end))',
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		keyframes: {
+    			fadeOut: {
+    				'0%': {
+    					opacity: '1'
+    				},
+    				'100%': {
+    					opacity: '0'
+    				}
+    			},
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			},
+    			glow: {
+    				'0%, 100%': {
+    					boxShadow: '0 4px 15px rgba(255, 0, 0, 0)'
+    				},
+    				'50%': {
+    					boxShadow: '0 4px 15px rgba(255, 0, 0, 0.6)'
+    				}
+    			},
+    			moveToBottom: {
+    				'0%': {
+    					transform: 'translateY(0)'
+    				},
+    				'100%': {
+    					transform: 'translateY(22vh)'
+    				}
+    			},
+    			fadeIn: {
+    				'0%': {
+    					opacity: '0',
+    					transform: 'scale(0.95)'
+    				},
+    				'100%': {
+    					opacity: '1',
+    					transform: 'scale(1)'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			glow: 'glow 10s infinite',
+    			fadeOut: 'fadeOut 3s 2s forwards',
+    			moveToBottom: 'moveToBottom 1s forwards',
+    			fadeIn: 'fadeIn 1s ease-out forwards'
+    		}
+    	}
+    },
+    plugins: [require('tailwindcss-animate')],
+} satisfies Config;
 
-export default config
+export default config;
