f0c0113a7095c93acabc4bcb90c0ac7fecca5ce6
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 02:15:17 +0200

    feat: Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,31 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import {ThemeProvider} from "@/components/theme-provider";
-import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
-import {AppSidebar} from "@/components/app-sidebar";
-import {Toaster} from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import React from 'react';
+import { ContextProvider } from '@/app/context';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 export const metadata: Metadata = {
-    title: "Akash Alchemist",
-    description: "AI Enhanced Image Generator",
-    keywords: "AI, Akash, Image Generation",
+    title: 'Akash Alchemist',
+    description: 'AI Enhanced Image Generator',
+    keywords: 'AI, Akash, Image Generation',
 };
 
-export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en" suppressHydrationWarning={true} className="overflow-hidden">
-        <body className='font-SFPro'>
-
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-            >
-
-                <SidebarProvider defaultOpen={false}>
-                    <AppSidebar/>
-                        {children}
+            <body className="font-SFPro bg-radial-gradient from-bg-gradient-start from-0% to-bg-gradient-end to-100% h-full w-full">
+                <ContextProvider>
+                    <main className='w-full'>
+                        <div className='grid grid-rows-[minmax(0,1fr)_minmax(0,max-content)] h-full w-full'>
+                            {children}
+                            <Footer/>
+                        </div>
+                    </main>
                     <Toaster/>
-                </SidebarProvider>
-
-            </ThemeProvider>
-        </body>
-</html>);
+                </ContextProvider>
+            </body>
+        </html>
+    );
 }
