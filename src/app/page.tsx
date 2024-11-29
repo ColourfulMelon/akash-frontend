f6cb113cacfaa6d34b3cbe412c1af5f7a332b506
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 02:14:25 +0200

    styles: Update home page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,20 @@ import Prompt from '@/components/Prompt';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import ImageContainer from '@/components/ImageContainer';
+import { toast } from '@/components/ui/use-toast';
 
 export default function Home() {
+    
     return (
-        <main className="items-center justify-center w-full ">
+        <div className='h-full'>
+            <Header/>
             
-            <div className="bg-gradient-to-b from-[#400002] to-background h-full">
-                <Header/>
+            <section className="flex flex-col justify-center align-middle h-full">
                 
-                <section className="flex flex-col justify-center align-middle h-full">
-                    
-                    <ImageContainer/>
-                    <Prompt/>
-                
-                </section>
-                <Footer/>
+                <ImageContainer/>
+                <Prompt/>
             
-            </div>
-        
-        
-        </main>
+            </section>
+        </div>
     );
 }
