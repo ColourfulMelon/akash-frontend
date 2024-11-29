617991b009992632b5ea7182b0a6c2f075b2ec3d
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 12:24:24 +0200

    chore: Update next config to allow all domain images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "standalone",
+	// todo find a better way to do this than whitelisting everything
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "**",
+			},
+			{
+				protocol: 'https',
+				hostname: 'placehold.co',
+			}
+		]
+	}
 };
 
 export default nextConfig;
