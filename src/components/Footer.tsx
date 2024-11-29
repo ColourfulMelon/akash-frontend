1acf2509b77182e05d97292b8d800853c8a05d07
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 02:16:21 +0200

    feat: Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,33 @@
-import Image from "next/image";
-import {Github, Heart} from "lucide-react";
-import {Separator} from "@/components/ui/separator";
+import Image from 'next/image';
+import { Github, Heart } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 
-export default function  Footer() {
-    return(
-        <section className='flex flex-col align-middle text-white w-full pb-4 text-center absolute bottom-0'>
-
-            <div className='flex justify-center mb-4'>
-                <Image src='/images/akash.svg' alt="akash" width={134} height={52}/>
-            </div>
-
-            <div className="flex mx-auto gap-4 mb-2">
+export default function Footer() {
+    return (
+        <section className="flex flex-col align-middle text-white w-full p-4 text-center">
+            
+            {/*<div className='flex justify-center mb-4'>*/}
+            {/*    <Image src='/images/akash-powered.svg' alt="akash" width={134} height={52}/>*/}
+            {/*</div>*/}
+            
+            <div className="flex mx-auto gap-2">
                 <a href="" target="_blank" className="underline">Docs</a>
-                <Separator orientation="vertical"/>
+                <Separator orientation="vertical" className='bg-primary-foreground'/>
                 <div className="flex">
                     <Github/>
-                    <a href="https://github.com/Dev3-Studio/akash-frontend" target="_blank" className="underline">Source Code</a>
+                    <a href="https://github.com/Dev3-Studio/akash-frontend" target="_blank" className="underline">
+                        Source Code
+                    </a>
                 </div>
-                <Separator orientation="vertical"/>
-                <p>Built with <Heart color="red" className="inline"/> by <a href="https://dev3.studio"
-                                                                            className="underline" target="_blank">Dev3
-                    Studio</a></p>
+                <Separator orientation="vertical" className='bg-primary-foreground'/>
+                <p>
+                    Built with <Heart color="red" className="inline"/> by
+                    <a href="https://dev3.studio" className="underline" target="_blank">Dev3 Studio</a>
+                </p>
             </div>
-
-            <p>Animated Icon by <a target="_blank" className="underline" href="https://lordicon.com/">Lordicon.com</a></p>
-
+            
+            {/*<p>Animated Icon by <a target="_blank" className="underline" href="https://lordicon.com/">Lordicon.com</a></p>*/}
+        
         </section>
-);
+    );
 }
