806e29de78a6765055cb2b608178ab096edb9063
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 05:32:25 +0200

    feat: Update AutoResizeTextarea

--- src/components/AutoResizeTextarea.tsx
+++ src/components/AutoResizeTextarea.tsx
@@ -1,140 +1,106 @@
 'use client';
-import React, {useEffect, useRef, useState} from 'react';
-import {Textarea} from "@/components/ui/textarea";
-import {useAtom} from "jotai/index";
-import {statusAtom} from "@/components/Prompt";
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+import { useImperativeHandle } from 'react';
 
-function AutoResizeTextarea() {
-    const [status, setStatus] = useAtom(statusAtom);
-
-    const calculateContentHeight = function( ta: HTMLTextAreaElement, scanAmount: number ) {
-        let height = ta.offsetHeight;
-        const origHeight = ta.style.height,
-            scrollHeight = ta.scrollHeight,
-            overflow = ta.style.overflow;
-        /// only bother if the ta is bigger than content
-        if ( height >= scrollHeight ) {
-            /// check that our browser supports changing dimension
-            /// calculations mid-way through a function call...
-            ta.style.height = (height + scanAmount) + 'px';
-            /// because the scrollbar can cause calculation problems
-            ta.style.overflow = 'hidden';
-            /// by checking that scrollHeight has updated
-            if ( scrollHeight < ta.scrollHeight ) {
-                /// now try and scan the ta's height downwards
-                /// until scrollHeight becomes larger than height
-                ta.style.height = ta.scrollHeight + 'px';
-
-                /// reset the ta back to its original height
-                ta.style.height = origHeight;
-                /// put the overflow back
-                ta.style.overflow = overflow;
-                return height;
-            }
-        } else {
-            return scrollHeight;
-        }
-    }
-
-    const calculateHeight = function(ta: HTMLTextAreaElement) {
-        // @ts-ignore
-        const style = (window.getComputedStyle) ? window.getComputedStyle(ta) : ta.currentStyle;
-
-        // This will get the line-height only if it is set in the css,
-        // otherwise it's "normal"
-        const taLineHeight = parseInt(style.lineHeight, 10);
-        // Get the scroll height of the textarea
-
-        const taHeight = calculateContentHeight(ta, taLineHeight);
-        if (!taHeight) return;
-        // calculate the number of lines
-        return Math.ceil(taHeight / taLineHeight);
-    };
-
-    const textareaRef = useRef(null);
-    const [value, setValue] = useState("");
-
-    useEffect(() => {
-        const textarea = textareaRef.current;
-
-        // @ts-ignore
-        textarea.style.height = 'auto';
-        // @ts-ignore
-        const numLines = calculateHeight(textarea);
-
-        // set height of textarea based on number of lines
-        // @ts-ignore
-        const style = (window.getComputedStyle) ? window.getComputedStyle(textarea) : textarea.currentStyle;
-        const taLineHeight = parseInt(style.lineHeight, 10);
-        // @ts-ignore
-        textarea.style.height = `${numLines * taLineHeight}px`;
-    }, [value]);
-
-    const typeSpeed = 50;
-
-    async function typeWriter(text: string){
-        setValue('');
-        let j = 0;
-
-        while (j <= text.length) {
-            setValue(text.slice(0, j));
-            j++;
-            await new Promise(r => setTimeout(r, typeSpeed));
-        }
-
-        setStatus({ ...status, status: 'generating' });
-    }
-
-
-    async function removeText() {
-        const textArea = textareaRef.current;
-        if (!textArea) return;
-        // @ts-ignore
-        textArea.placeholder = 'Generating prompt...';
-        // @ts-ignore
-        textArea.disabled = true;
-
-        let i = 0;
-        while (i <= value.length) {
-            setValue(value.slice(0, value.length - i));
-            i++;
-            await new Promise(r => setTimeout(r, typeSpeed));
-        }
-    }
-
-
-    // @ts-ignore
-    const handleChange = (event) => {
-        console.log('Update value to ', event.target.value);
-        setValue(event.target.value);
-    };
-
-    useEffect(() => {
-        if (status.status === 'idle') return;
-        if (status.status === 'typing') {
-            if (status.prompt === ''){
-                console.log(value);
-                removeText();
-            } else {
-                typeWriter(status.prompt);
-            }
-        }
-
-    }, [status]);
-
-    return (
-        <div className="w-full">
-            <Textarea
-                rows={1}
-                ref={textareaRef}
-                value={value}
-                onChange={handleChange}
-                className="w-full border-none !ring-0 resize-none scrollbar-hidden overflow-y-scroll p-0 max-h-40 text-white placeholder:text-white font-light text-xl disabled:opacity-80"
-                placeholder="Type your prompt here"
-            />
-
-        </div>
-    );
+interface UseAutosizeTextAreaProps {
+    textAreaRef: React.MutableRefObject<HTMLTextAreaElement | null>;
+    minHeight?: number;
+    maxHeight?: number;
+    triggerAutoSize: string;
 }
 
-export default AutoResizeTextarea;
+export const useAutosizeTextArea = ({
+                                        textAreaRef,
+                                        triggerAutoSize,
+                                        maxHeight = Number.MAX_SAFE_INTEGER,
+                                        minHeight = 0,
+                                    }: UseAutosizeTextAreaProps) => {
+    const [init, setInit] = React.useState(true);
+    React.useEffect(() => {
+        // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+        const offsetBorder = 6;
+        const textAreaElement = textAreaRef.current;
+        if (textAreaElement) {
+            if (init) {
+                textAreaElement.style.minHeight = `${minHeight + offsetBorder}px`;
+                if (maxHeight > minHeight) {
+                    textAreaElement.style.maxHeight = `${maxHeight}px`;
+                }
+                setInit(false);
+            }
+            textAreaElement.style.height = `${minHeight + offsetBorder}px`;
+            const scrollHeight = textAreaElement.scrollHeight;
+            // We then set the height directly, outside of the render loop
+            // Trying to set this with state or a ref will product an incorrect value.
+            if (scrollHeight > maxHeight) {
+                textAreaElement.style.height = `${maxHeight}px`;
+            } else {
+                textAreaElement.style.height = `${scrollHeight + offsetBorder}px`;
+            }
+        }
+    }, [textAreaRef.current, triggerAutoSize]);
+};
+
+export type AutosizeTextAreaRef = {
+    textArea: HTMLTextAreaElement;
+    maxHeight: number;
+    minHeight: number;
+};
+
+type AutosizeTextAreaProps = {
+    maxHeight?: number;
+    minHeight?: number;
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+export const AutosizeTextarea = React.forwardRef<AutosizeTextAreaRef, AutosizeTextAreaProps>(
+    (
+        {
+            maxHeight = Number.MAX_SAFE_INTEGER,
+            minHeight = 52,
+            className,
+            onChange,
+            value,
+            ...props
+        }: AutosizeTextAreaProps,
+        ref: React.Ref<AutosizeTextAreaRef>,
+    ) => {
+        const textAreaRef = React.useRef<HTMLTextAreaElement | null>(null);
+        const [triggerAutoSize, setTriggerAutoSize] = React.useState('');
+        
+        useAutosizeTextArea({
+            textAreaRef,
+            triggerAutoSize: triggerAutoSize,
+            maxHeight,
+            minHeight,
+        });
+        
+        useImperativeHandle(ref, () => ({
+            textArea: textAreaRef.current as HTMLTextAreaElement,
+            focus: () => textAreaRef?.current?.focus(),
+            maxHeight,
+            minHeight,
+        }));
+        
+        React.useEffect(() => {
+            setTriggerAutoSize(value as string);
+        }, [props?.defaultValue, value]);
+        
+        return (
+            <textarea
+                {...props}
+                value={value}
+                ref={textAreaRef}
+                className={cn(
+                    'flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                    className,
+                )}
+                onChange={(e) => {
+                    setTriggerAutoSize(e.target.value);
+                    onChange?.(e);
+                }}
+            />
+        );
+    },
+);
+AutosizeTextarea.displayName = 'AutosizeTextarea';
