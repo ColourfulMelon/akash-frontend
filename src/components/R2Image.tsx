1207e361c4818b557356a7db32dc155e8d59d5ee
ColourfulMelon <jordynnitch@gmail.com>
2024-11-30 11:05:53 +0200

    add explore page, update history page to use content

--- src/components/R2Image.tsx
+++ src/components/R2Image.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import {PromptResult} from "@/lib/zodSchemas";
 
-export default function R2Image({fileName}: { fileName: string }) {
-	const imgURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/outputs/${fileName}`
+export default function R2Image({file}: { file: PromptResult }) {
+	const imgURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/outputs/${file.outputFilename}`;
 
 	return (
 		<div>
 			<Image src={imgURL}
-			       alt={fileName}
+			       alt={file.text}
 			       width={832}
 			       height={1216}
 			/>
