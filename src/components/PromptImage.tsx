6cf761faab509f746d86f03d136f7efc2da05251
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-30 18:34:49 +0200

    wip: Add specific width/height to prompt images

--- src/components/PromptImage.tsx
+++ src/components/PromptImage.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image';
 import { PromptResult } from '@/lib/zodSchemas';
 
-export default function PromptImage({ file }: { file: PromptResult }) {
-	const imgURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/outputs/${file.outputFilename}`;
-
+export default function PromptImage({ promptResult }: { promptResult: PromptResult }) {
+	const imgURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/outputs/${promptResult.outputFilename}`;
+	const DIMENSIONS = {
+		landscape: { width: 1216, height: 832 },
+		portrait: { width: 832, height: 1216 },
+		square: { width: 1024, height: 1024 },
+	};
 	return (
 		<div>
 			<Image src={imgURL}
-			       alt={file.text}
-			       width={832}
-			       height={1216}
+			       alt={promptResult.text}
+			       width={DIMENSIONS[promptResult.layout].width}
+			       height={DIMENSIONS[promptResult.layout].height}
 			/>
 		</div>
 	);
