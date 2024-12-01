6fc675ccf199145b063a1e0a6ec4d1aafde1460f
ColourfulMelon <mclovinmohammed1981@gmail.com>
2024-12-01 17:23:07 +0200

    footer styling fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,20 +5,20 @@ import { cn } from '@/lib/utils';
 
 export default function Footer({ className }: { className?: string }) {
     return (
-        <section className={cn("flex flex-col align-middle text-white w-full p-4 text-center absolute bottom-5", className)}>
+        <section className={cn("flex flex-col align-middle text-white w-full p-4 text-center absolute bottom-2", className)}>
 
             {/*<div className='flex justify-center mb-4'>*/}
             {/*    <Image src='/images/akash-powered.svg' alt="akash" width={134} height={52}/>*/}
             {/*</div>*/}
 
-            <div className="flex mx-auto gap-2 text-sm sm:text-base">
+            <div className="flex mx-auto gap-2 text-sm sm:text-base h-5">
                 <div className="flex">
                     <Github/>
                     <a href="https://github.com/Dev3-Studio/Akash-Alchemist" target="_blank" className="underline">
                         Source Code
                     </a>
                 </div>
-                <Separator orientation="vertical" className='bg-primary-foreground'/>
+                <Separator orientation="vertical" className='bg-primary-foreground h-full'/>
                 <p>
                     Built with <Heart color="red" className="inline"/> by
                     <a href="https://dev3.studio" className="underline" target="_blank">Dev3 Studio</a>
