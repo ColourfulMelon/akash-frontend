20876a70ef5607eb5942c03ef43f81904049f43b
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-12-01 20:59:37 +0200

    feat: Add page title with optional toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,11 @@
 import { PromptHistorySheet } from '@/components/PromptHistorySheet';
 import { useSidebar } from '@/components/ui/sidebar';
 import { Menu } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
-export default function Header() {
+export default function Header({ disableTitle }: { disableTitle?: boolean }) {
+    const pathname = usePathname();
     const { setOpen, toggleSidebar, isMobile } = useSidebar();
     
     function openSidebar() {
@@ -14,7 +17,12 @@ export default function Header() {
     
     return (
         <header className="flex justify-between items-center w-full py-4 px-4 md:px-8">
-            {/*mobile sidebar toggle*/}
+            <div className={cn(
+                'hidden lg:block text-4xl',
+                disableTitle ? 'hidden' : '',
+            )}>
+                {pathname.charAt(1).toUpperCase() + pathname.slice(2)}
+            </div>
             <Menu className="cursor-pointer lg:invisible" onClick={openSidebar}/>
             <PromptHistorySheet/>
         </header>);
