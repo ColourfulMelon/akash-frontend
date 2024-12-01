6a3aa8e3012564f7b649ce26c4a3e599a563dba5
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-12-01 22:09:23 +0200

    wip: Fix layout

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -15,13 +15,14 @@ import { Check } from 'lucide-react';
 import { Spinner } from '@/components/ui/Spinner';
 import PromptImage from '@/components/PromptImage';
 import { PromptProgressBar } from '@/components/PromptProgressBar';
+import PromptInput from '@/components/PromptInput';
 
 export default function Playground() {
     const params = useSearchParams();
     const router = useRouter();
     const clientId = useClientId();
     const [isCreatingPrompt, setIsCreatingPrompt] = useState(false);
-
+    
     const pendingPrompts = useQuery({
         queryKey: ['getAllPromptResults', clientId, PromptStatus.Pending],
         queryFn: async () => {
@@ -34,7 +35,7 @@ export default function Playground() {
         refetchInterval: 3000,
         enabled: clientId !== null,
     });
-
+    
     const lastPrompt = useQuery({
         queryKey: ['getAllPromptResults', clientId, 1],
         queryFn: async () => {
@@ -51,7 +52,7 @@ export default function Playground() {
         refetchInterval: 1000,
         enabled: clientId !== null,
     });
-
+    
     const createPromptMutation = useMutation({
         mutationFn: async (prompt: PromptCreate) => {
             if (prompt.enhanceText || (!prompt.layoutOverride && !prompt.workflowOverride && !prompt.seedOverride)) {
@@ -91,12 +92,12 @@ export default function Playground() {
             if (params.get('start') === 'true') router.replace('/playground');
         },
     });
-
+    
     const handleCreatePrompt = async (prompt: PromptCreate) => {
         setIsCreatingPrompt(true);
         await createPromptMutation.mutateAsync(prompt);
     };
-
+    
     useEffect(() => {
         const paramsSchema = z.object({
             clientId: z.string().uuid(),
@@ -122,53 +123,54 @@ export default function Playground() {
             return;
         }
         const data = parsed.data;
-
+        
         if (data.start) createPromptMutation.mutate(parsed.data);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [clientId]);
-
+    
     return (
-        <div className="grid grid-rows-[minmax(0,max-content)_minmax(0,1fr)]">
-            <div className="flex px-8 pt-4 pb-2">
-                <div className="flex flex-col h-full gap-8 w-80 min-h-full">
-                    <div className="flex flex-shrink">
-                        <PromptCreateCard
-                            defaultEnhanceText={true}
-                            disableSubmit={isCreatingPrompt}
-                            onSubmit={handleCreatePrompt}
-                        />
-                    </div>
-                    <div className="flex flex-grow">
-                        <Card className="h-fit w-full">
-                            <CardHeader>
-                                <CardTitle>Queue</CardTitle>
-                            </CardHeader>
-                            <CardContent className="h-[300px] overflow-y-auto space-y-4 px-3">
-                                {pendingPrompts.isLoading && <Skeleton className="w-full h-24"/>}
-                                {pendingPrompts.data?.map((result) => (
-                                    <div key={result.promptId}
-                                         className="flex flex-col gap-1 border rounded-md py-2 px-4">
-                                        <div className="mb-2 line-clamp-1">{result.text}</div>
-                                        <PromptProgressBar progress={result.progress ?? 0}
-                                                           message={result.statusMessage ?? 'In queue'}/>
-                                    </div>
-                                ))}
-                                {pendingPrompts.data?.length === 0 &&
-                                    <div className="text-muted-foreground">Start generating images to see them
-                                        here</div>}
-                            </CardContent>
-                        </Card>
-                    </div>
+        <div className="grid w-full
+            grid-rows-[minmax(0,1fr)_minmax(0,max-content)]
+            2xl:grid-cols-[minmax(0,max-content)_minmax(0,1fr)]
+            ">
+            <div className="hidden 2xl:flex flex-col h-full gap-8 w-80">
+                <div className="flex flex-shrink">
+                    <PromptCreateCard
+                        defaultEnhanceText={true}
+                        disableSubmit={isCreatingPrompt}
+                        onSubmit={handleCreatePrompt}
+                    />
                 </div>
-                <div className="flex w-full">
-                    <div className="m-auto">
-                        {lastPrompt.data && <div className="flex flex-row gap-2">
-                            <div className="flex flex-col items-center gap-2 max-w-screen-md">
-                                <PromptImage promptResult={lastPrompt.data}/>
-                            </div>
-                        </div>}
-                    </div>
+                <div className="flex flex-grow">
+                    <Card className="h-fit w-full">
+                        <CardHeader>
+                            <CardTitle>Queue</CardTitle>
+                        </CardHeader>
+                        <CardContent className="h-[300px] overflow-y-auto space-y-4 px-3">
+                            {pendingPrompts.isLoading && <Skeleton className="w-full h-24"/>}
+                            {pendingPrompts.data?.map((result) => (
+                                <div key={result.promptId}
+                                     className="flex flex-col gap-1 border rounded-md py-2 px-4">
+                                    <div className="mb-2 line-clamp-1">{result.text}</div>
+                                    <PromptProgressBar progress={result.progress ?? 0}
+                                                       message={result.statusMessage ?? 'In queue'}/>
+                                </div>
+                            ))}
+                            {pendingPrompts.data?.length === 0 &&
+                                <div className="text-muted-foreground">Start generating images to see them
+                                    here</div>}
+                        </CardContent>
+                    </Card>
                 </div>
+            </div>
+            <div className="flex w-full h-full p-8">
+                <div className="m-auto w-screen-sm">
+                    {/*todo: Figure out how to size this responsively*/}
+                    {lastPrompt.data && <PromptImage promptResult={lastPrompt.data}/>}
+                </div>
+            </div>
+            <div className="flex justify-center 2xl:hidden">
+                <PromptInput onSubmit={handleCreatePrompt}/>
             </div>
         </div>
     );
