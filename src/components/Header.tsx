ff15a87e4aa34ab3beb85f8a1b7f26e3d6deb54d
ColourfulMelon <mclovinmohammed1981@gmail.com>
2024-12-01 17:20:11 +0200

    mobile sidebar fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,18 @@ import {Button} from "@/components/ui/button";
 import {Menu} from "lucide-react";
 
 export default function Header(){
-	const { toggleSidebar } = useSidebar();
+	const { setOpen, toggleSidebar, isMobile } = useSidebar();
+
+	function openSidebar(){
+		toggleSidebar();
+		// fix text on mobile sidebar
+		isMobile && setOpen(true);
+	}
 
 	return (
 		<header className='flex justify-between items-center w-full py-4 px-4 md:px-8'>
 			{/*mobile sidebar toggle*/}
-			<Button className="" onClick={toggleSidebar}><Menu /></Button>
+			<Button className="" onClick={openSidebar}><Menu /></Button>
 
 			<PromptHistorySheet/>
 		</header>)
