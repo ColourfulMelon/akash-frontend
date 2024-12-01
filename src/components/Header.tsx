7f641b091daf2261cb9aa78cbd58cd4616cf9ad8
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-12-01 20:45:34 +0200

    refactor: Clean up header code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,21 @@
 'use client';
-import {PromptHistorySheet} from "@/components/PromptHistorySheet";
-import {useSidebar} from "@/components/ui/sidebar";
-import {Button} from "@/components/ui/button";
-import {Menu} from "lucide-react";
+import { PromptHistorySheet } from '@/components/PromptHistorySheet';
+import { useSidebar } from '@/components/ui/sidebar';
+import { Menu } from 'lucide-react';
 
-export default function Header(){
-	const { setOpen, toggleSidebar, isMobile } = useSidebar();
-
-	function openSidebar(){
-		toggleSidebar();
-		// fix text on mobile sidebar
-		isMobile && setOpen(true);
-	}
-
-	return (
-		<header className='flex justify-between items-center w-full py-4 px-4 md:px-8'>
-			{/*mobile sidebar toggle*/}
-			<Button className="" onClick={openSidebar}><Menu /></Button>
-
-			<PromptHistorySheet/>
-		</header>)
+export default function Header() {
+    const { setOpen, toggleSidebar, isMobile } = useSidebar();
+    
+    function openSidebar() {
+        toggleSidebar();
+        // fix text on mobile sidebar
+        if (isMobile) setOpen(true);
+    }
+    
+    return (
+        <header className="flex justify-between items-center w-full py-4 px-4 md:px-8">
+            {/*mobile sidebar toggle*/}
+            <Menu className="cursor-pointer lg:invisible" onClick={openSidebar}/>
+            <PromptHistorySheet/>
+        </header>);
 }
